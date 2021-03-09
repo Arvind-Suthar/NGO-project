37ab9f4f4f8fb045aab62592e321c6665de008f9
Arvind-Suthar <35096203+Arvind-Suthar@users.noreply.github.com>
2021-03-09 18:12:34 +0530

    Final completed site

--- NGO project/js/menuToggle.js
+++ NGO project/js/menuToggle.js
@@ -1,7 +1,29 @@
 var menuActive = false;
 var navbar = document.querySelector("#nav");
-console.log(navbar);
 var toggle = () => {
     navbar.classList.toggle("active");
-    console.log(navbar.classList);
 };
+
+
+var currActiveMenu = "#donateAccordian";
+
+$("#faq-selector").change(function () {
+    if($(this).val() == 1){
+        document.querySelector(currActiveMenu).classList.add("hide");
+        document.querySelector("#donateAccordian").classList.toggle("hide");
+        currActiveMenu = "#donateAccordian";
+    }else if($(this).val() == 2){
+        document.querySelector(currActiveMenu).classList.add("hide");
+        document.querySelector("#supportAccordian").classList.toggle("hide");
+        currActiveMenu = "#supportAccordian";
+    }else if($(this).val() == 3){
+        document.querySelector(currActiveMenu).classList.add("hide");
+        document.querySelector("#volunteerAccordian").classList.toggle("hide");
+        currActiveMenu = "#volunteerAccordian";
+    }else{
+
+    }
+    /*var menu = document.querySelector("#" + menuId);
+    var currmenu = document.querySelector("#" + currActiveMenu);
+    menu.classList.toggle("hide");*/
+});
