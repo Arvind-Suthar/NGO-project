e7b71f9bcf55ba8ed7b7dd8c86df42a9035ac1e4
Arvind Suthar <35096203+Arvind-Suthar@users.noreply.github.com>
2021-03-30 18:12:25 +0530

    Delete Pevok - complete/Pevok - complete/pevok directory

--- Pevok - complete/Pevok - complete/pevok/public/js/mailApproving.js
+++
@@ -1,8 +0,0 @@
-let approveButtons = document.querySelectorAll(".approve-btn");
-
-
-
-approveButtons.forEach(el => el.addEventListener('click', event => {
-    let id = el.getAttribute("data-approve-id");
-    window.location.replace("http://localhost:3000/approveMails/approve/" + id);
-}));
